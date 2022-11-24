85d39bd7a4098ecb1df29cfd29b6c0afca39bf2c
Zartab M Nakhwa <zartab@codewithz.com>
2022-11-24 19:41:37 +0530

    Initial commit

---
+++ src/components/WithdrawDepositComponent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function WithdrawDepositComponent() {
+  return (
+    <div className="container" style={{ marginTop: "50px" }}>
+      <button className="btn btn-info">Deposit $100</button>
+      <br />
+      <br />
+      <br />
+      <button className="btn btn-danger">Withdraw $100</button>
+    </div>
+  );
+}
